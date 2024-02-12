d14ee5f1613721f1670fe0635eaa3afbae4d52f1
mithul.se <Mithul@nextgenitltd.com>
2024-02-12 23:25:07 +0600

    validation errors fix

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,17 +5,27 @@ namespace App\Http\Controllers;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
     public function login(Request $request)
     {
-        // Validate user credentials (customize as needed)
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
 
         if ($token = JWTAuth::attempt($request->only('email', 'password'))) {
             return response()->json(compact('token'));
         }
 
-        return response()->json(['error' => 'Invalid credentials'], 401);
+        return response()->json([
+            'error' => 'Unauthorized'
+        ], 401);
     }
 }
